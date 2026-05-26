ce4fd113de6a8be768db31f3fd5e7105d1c6e390
ksaisindhura <ksaisindhura83@gmail.com>
2026-05-26 18:11:20 +0530

    2nd commit

---
+++ src/test/java/testCases/TC004_Search.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+import utilities.DataProviders;
+
+public class TC004_Search extends BaseClass {
+	//later
+}
